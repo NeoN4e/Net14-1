037065a3dcab52479886c57464823a04b1815bd8
Vasya Pypkin <vasy@ukr.net>
2014-10-07 16:49:32 +0300

    Casino

---
+++ Casino/Casino/GameLogic.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Table
+    {
+        /// <summary>Коллекция игроков текущего стола</summary>
+        public List<Player> PlayerList = new List<Player>();
+
+        /// <summary>Коллекция карт текущего стола</summary>
+        List<Cart> CartPool;
+
+        /// <summary>Берет из колоды карту</summary>
+        public Cart GetNextCart()
+        {
+            int index = MyRandom.R.Next(0, this.CartPool.Count);
+
+            try { return this.CartPool[index]; }
+            finally 
+            { 
+                //Удалим карту из пула
+                this.CartPool.RemoveAt(index); 
+            }
+        }
+
+        /// <summary>инициализируеться новая колода и всем игрокам по 2 карты</summary>
+        public void StartGame()
+        {
+            //Инициализация колоды
+            this.CartPool = new List<Cart>();
+
+            for (int i = 2; i <= 11; i++)
+            {
+                this.CartPool.Add(new Cart(i));//4 масти от 2-х до 11
+                this.CartPool.Add(new Cart(i));
+                this.CartPool.Add(new Cart(i));
+                this.CartPool.Add(new Cart(i));
+            }
+
+            //Выдадим по 2- стартовые карты всем игрокам
+            foreach (Player p in PlayerList)
+            {
+                p.AddCard(GetNextCart());
+                p.AddCard(GetNextCart());
+            }
+
+
+        }
+
+        /// <summary>По очереди ходят все ии и определяеться победитель</summary>
+        public void EndGame()
+        {
+            //По очереди ходят все игроки
+            foreach (Player pc in PlayerList)
+            {
+                /// <summary>Логика хода ИИ</summary>
+                if (!pc.isHuman())
+                {
+                    while (pc.Score < 17)
+                    {
+                        pc.AddCard(GetNextCart());
+                    }
+                }
+            }
+        }
+    }
+}
--- Casino/Casino/MainWindow.xaml.cs
+++ Casino/Casino/MainWindow.xaml.cs
@@ -15,15 +15,47 @@ using System.Windows.Shapes;
 
 namespace Casino
 {
-
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        Table table;
+        Player human;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        
+        private void StartGame(object sender, RoutedEventArgs re)
+        {
+            //Создадим стол
+            this.table = new Table();
+
+            //Добавим игроков
+            this.human = new Player(PlayerType.Human);
+            human.OnCartAdd     += (p, e) => { this.PlayerScore.Content = p.Score; };
+            human.OnOverflow    += (p, c) => { this.PlayerScore.Foreground = Brushes.Red;};
+            human.On21Score     += (p, c) => { this.PlayerScore.Foreground = Brushes.Green; };
+
+            this.table.PlayerList.Add(human);
+
+            Player Casino = new Player(PlayerType.PC);
+            Casino.OnCartAdd += (p, e) => { this.CasinoScore.Content = p.Score; };
+            this.table.PlayerList.Add(Casino);
+ 
+            //Начнем
+            table.StartGame();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            human.AddCard( table.GetNextCart());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            table.EndGame();
         }
     }
 }
