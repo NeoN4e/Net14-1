69017de459e0b0219fb37d491dbff4e694153122
Vasya <Vasya@ya.ru>
2014-10-15 20:54:51 +0300

    no message

---
+++ CubikProject/CubikProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubikProject
+{
+    class Program
+    {
+        static Random r = new Random();
+        static int ShowCubik()
+        {
+            int number = r.Next(1, 7);
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
