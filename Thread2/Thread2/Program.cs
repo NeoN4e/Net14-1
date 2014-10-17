cedbed2eca942838a51752149ec7080fdb8e34a9
Vasya <Vasya@ya.ru>
2014-10-17 20:47:57 +0300

    17 10 2014

---
+++ Thread2/Thread2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread2
+{
+    class Program
+    {
+            //1. Создать несколько потоков, выводящих на консоль в цикле свой hashcode. Посмотреть порядок вывода.
+            //Добавить Sleep(1), Sleep(10), Sleep(0). Посмотреть изменения в порядке вывода. Закоментировать Sleep() и установить приоритеты потокам. Посмотреть изменения в порядке вывода.
+            //2. Написать программу, демонстрирующую работу Join(): основной поток должен дожидаться окончания работы (не менее) 2-х потоков, потом продолжать работу. 
+            //Конструктивно: дополнительные потоки должны предоставлять данные, необходимые для дальнейшей работы основного потока.
+        static void Go(object ob)
+        {
+            Console.WriteLine("Start thread {0}", ob);
+            for (int i = 0; i < 10; i++)
+            {
+               // Thread.Sleep(0);
+                Console.WriteLine("    Thread {0} priority {1}", ob,Thread.CurrentThread.Priority);
+            }
+
+            Console.WriteLine("END thread {0}",ob);
+        }
+        static Random r = new Random();
+
+        static int Rezalt = 0;
+        static void Sum(object ob)
+        {
+            int rezalt=0;
+            for (int i = (int)ob ; i < ((int)ob) + 10; i++)
+            {
+                rezalt += i;
+            }
+
+            lock((object)Rezalt)
+            { Rezalt += rezalt; }
+        }
+
+        static void Main(string[] args)
+        {
+            //Thread item;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    item = new Thread(Go);
+            //    //item.Priority = (ThreadPriority)(4-i);
+            //    //item.Priority = (ThreadPriority)r.Next(5);
+            //    item.Start(i);
+            //}
+
+
+            //Go("main");
+
+            Thread t1 = new Thread(Sum);
+            Thread t2 = new Thread(Sum);
+
+    
+                t1.Start(1); 
+                t2.Start(2);
+
+                Console.WriteLine(t2.ThreadState);
+                t2.Join();
+                Thread.Sleep(1000);
+                Console.WriteLine(t1.ThreadState);
+                t1.Join();
+            //Sum(1);
+            //Sum(2);
+            Console.WriteLine(Rezalt);
+        }
+    }
+}
---
+++ ThreadGame/ThreadGame/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadGame
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //static Button Mouse;
+     
+
+        class test : System.ComponentModel.INotifyPropertyChanged
+        {
+            Thickness th;
+            public Thickness Th 
+            { 
+                get{return th;}
+                set
+                {
+                    th = value;
+                    if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Th"));
+                }
+            }
+
+            public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            test tes = new test();
+
+            Button Mouse = new Button();
+            Mouse.Height = 30;
+            Mouse.Width = 30;
+
+            Binding bind = new Binding();
+            bind.Path = new PropertyPath("Th");
+            bind.Source = tes;
+            Mouse.SetBinding(Button.MarginProperty, bind);
+
+            this.MainGrid.Children.Add(Mouse);
+
+
+            
+            
+            try
+            {
+                Thread t = new Thread(() =>
+                    {
+                        try
+                        {
+                            //Mouse.Margin = new Thickness(Mouse.Margin.Left + 100);
+                            //Th = new Thickness(100);
+                            for (int i = 0; i < 100; i++)
+                            {
+                                tes.Th = new Thickness(tes.Th.Left + 10);
+                                Thread.Sleep(100);
+                            }
+                            
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    });
+                t.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+        }
+    }
+}
