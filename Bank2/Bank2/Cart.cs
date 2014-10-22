66d63a0696ce87965ce9bd18ce4162d327d907c3
Vasya <Vasya@ya.ru>
2014-10-22 20:53:34 +0300

    22 10 2014

---
+++ Bank2/Bank2/Cart.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    class BankAccount : INotifyPropertyChanged
+    {
+        public static ObservableCollection<BankAccount> obslist = new ObservableCollection<BankAccount>();
+
+        public int CartId{get;private set;}
+
+        double summ;
+        public double Summ
+        {
+            get { return this.summ; } 
+            private set
+            {
+                //Заблокируем счет
+                Mutex mutex = new Mutex(false, "BankAccount" + CartId.ToString());
+                mutex.WaitOne();
+
+                Thread.Sleep(2000);
+
+                //Проверим достаточно ли средств
+                if ((this.summ + value) < 0) throw new NotEnoughMoneyException("Нелостаточно денег");
+                this.summ = value;
+
+                //Разблокируем
+                mutex.ReleaseMutex();
+
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Summ"));
+            }
+            
+        }
+
+        public BankAccount(int id)
+        {
+            this.CartId = id;
+
+            obslist.Add(this);
+        }
+
+        /// <summary>Вносит удаляет деньги со счета</summary>
+        public bool add(double money)
+        {
+            Summ += money;
+            return true;
+        }
+
+        
+        public class NotEnoughMoneyException: ApplicationException
+        {
+            public NotEnoughMoneyException(string msg) : base(msg) { }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Bank2/Bank2/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bank2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(String name)
+        {
+            InitializeComponent();
+
+            Binding bind = new Binding();
+            bind.Source = BankAccount.obslist;
+            this.listbox.SetBinding(ListBox.ItemsSourceProperty, bind);
+
+            //if (name != "Main window")
+            //{ new MainWindow("Second window").Show();}
+
+            this.Title = name;
+         }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Binding bind = new Binding();
+            bind.Source = BankAccount.obslist;
+            this.listbox.SetBinding(ListBox.ItemsSourceProperty, bind);
+
+            this.Title = "Main window";
+
+            //Dispatcher.BeginInvoke(new (Delegate)delegate(){});
+            //Dispatcher. BeginInvoke( delegate()
+           // {
+           //     new MainWindow("Second window").Show();
+           // });
+                //Thread t = new Thread(() =>
+                //{
+                //    try
+                //    {
+                      new MainWindow("Second window").Show();
+            //System.Windows.Threading.Dispatcher.CurrentDispatcher.BeginInvoke(new (System.Delegate)delegate(() => new MainWindow("Second window").Show()));
+                //        System.Windows.Threading.Dispatcher.Run();
+                //    }
+                //    catch (Exception ex)
+                //    { MessageBox.Show(ex.Message); }
+                //}
+                //);
+                //t.SetApartmentState(ApartmentState.STA);
+                //t.Start();
+            // System.Windows.Threading.Dispatcher.Run();
+                
+
+            //Dispatcher.BeginInvoke(new Action(delegate()
+            //{
+            //    new MainWindow("Second window").Show();
+            //}));
+        }
+
+
+        private void AddAccount(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Создадим Новый счет
+                new BankAccount(Convert.ToInt32(this.Input.Text));
+
+                //MessageBox.Show(String.Format("{0:#.00}", 11234567889));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AddMoney(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BankAccount Shet = (BankAccount)this.listbox.SelectedItem;
+                Shet.add(Convert.ToDouble(this.AddMoneySumm.Text.Replace(".",",")));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
